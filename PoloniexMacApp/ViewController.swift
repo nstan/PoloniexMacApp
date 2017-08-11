1a81d8cd4323670111a128b9b8b90b16c2713c5e
Nikola Stan <stan.nikola@gmail.com>
2017-08-11 01:50:54 -0600

    Initial Commit

---
+++ PoloniexMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PoloniexMacApp
+//
+//  Created by Nikola Stan on 8/11/17.
+//  Copyright © 2017 Nikola Stan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
